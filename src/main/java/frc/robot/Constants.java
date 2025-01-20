a2a9382cc324ca7506393bb1d9889482d5fed176
lleonidik228 <ianadivane2006@gmail.com>
2025-01-20 21:31:33 +0200

    improved pid for autonomy and settingsfor robot and pathplaner.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public final class Constants
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
         // Maximum speed of the robot in meters per second, used to limit acceleration.
-  public static final double MAX_SPEED  = Units.feetToMeters(20); //v14.5
+  public static final double MAX_SPEED  = Units.feetToMeters(10); //v14.5
 
   public static final class AutonConstants
   { 
@@ -47,7 +47,7 @@ public final class Constants
     public static final double LEFT_X_DEADBAND  = 0.1;
     public static final double LEFT_Y_DEADBAND  = 0.1;
     public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT    = 6; //6 
+    public static final double TURN_CONSTANT    = 2; //6 
     
   }
 
