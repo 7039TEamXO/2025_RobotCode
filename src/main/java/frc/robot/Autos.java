358a254534beae9175d02d4218553dd70d4a01cf
lleonidik228 <ianadivane2006@gmail.com>
2025-03-04 22:20:28 +0200

    Copied autonomies to the red side and finished the two and one algae autos.

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -2,15 +2,16 @@ package frc.robot;
 
 public enum Autos {
     Test("Test"),
-    Safe_Right("Safe_Right"),
-    Level_4_Right("Level_4_Right"),
     Safe_Left("Safe_Left"),
-    Level_4_Left("Level_4_Left"),
-    Double_Algae_Right("Double_Algae_Right"),
+    Safe_Right("Safe_Right"),
+    Level_4_Left_Red("Level_4_Left_Red"),
+    Level_4_Left_Blue("Level_4_Left_Blue"),
+    Level_4_Right_Red("Level_4_Right_Red"),
+    Level_4_Right_Blue("Level_4_Right_Blue"),
     Double_Algae_Left("Double_Algae_Left"),
-    CoralAndAlgaeCenter("CoralAndAlgaeCenter");
-    
-
+    Double_Algae_Right("Double_Algae_Right"),
+    Coral_Algae_Left("Coral_Algae_Left"),
+    Coral_Algae_Right("Coral_Algae_Right");
     
     private final String autoName;
     
